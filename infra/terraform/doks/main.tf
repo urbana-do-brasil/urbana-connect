2632f72d39e89ac70dcc46254bbb667348ae8856
Emanuel Brito <emanuelguimaraes23@gmail.com>
2025-04-16 15:11:10 -0300

    feat: atualiza versão do Kubernetes no Terraform para usar a versão mais recente recomendada

--- infra/terraform/doks/main.tf
+++ infra/terraform/doks/main.tf
@@ -31,10 +31,17 @@ variable "cluster_name" {
   default     = "urbana-connect-cluster"
 }
 
+# Data source para buscar as versões K8s disponíveis/recomendadas
+data "digitalocean_kubernetes_versions" "latest" {
+  # Você pode adicionar version_prefix se quiser uma versão específica major.minor, ex: "1.28"
+  # version_prefix = "1.29" 
+}
+
 resource "digitalocean_kubernetes_cluster" "primary" {
   name    = var.cluster_name
   region  = var.do_region
-  version = "1.29.1-do.0"
+  # Usar a versão mais recente recomendada obtida pelo data source
+  version = data.digitalocean_kubernetes_versions.latest.latest_version
 
   node_pool {
     name       = "default-pool"
