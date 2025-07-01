065fd32539be49b1f5ef40a9299ef37a71de709d
Emanuel Brito <emanuelguimaraes23@gmail.com>
2025-07-01 16:03:41 -0300

    fix: adicionando o repositório no Helm na pipeline

--- infra/terraform/02-kube-addons/prometheus.tf
+++ infra/terraform/02-kube-addons/prometheus.tf
@@ -1,9 +1,8 @@
 resource "helm_release" "prometheus" {
   name       = "prometheus"
-  repository = "https://prometheus-community.github.io/helm-charts"
-  chart      = "prometheus"
+  chart      = "prometheus-community/prometheus"
   namespace  = kubernetes_namespace.observability.metadata[0].name
-  version    = "25.20.0"
+  version    = "25.20.0" # Versão estável para evitar atualizações inesperadas
 
   values = [
     file("${path.module}/prometheus/values.yaml")
