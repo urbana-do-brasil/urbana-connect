4531e023e218edd8f0242b8e73fade36a3b3c4d5
Emanuel Brito <emanuelguimaraes23@gmail.com>
2025-07-01 16:18:23 -0300

    fix: torna a definição do chart helm auto-contida no terraform

--- infra/terraform/02-kube-addons/prometheus.tf
+++ infra/terraform/02-kube-addons/prometheus.tf
@@ -1,6 +1,7 @@
 resource "helm_release" "prometheus" {
   name       = "prometheus"
-  chart      = "prometheus-community/prometheus"
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "prometheus"
   namespace  = kubernetes_namespace.observability.metadata[0].name
   version    = "25.20.0" # Versão estável para evitar atualizações inesperadas
 
